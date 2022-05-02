dfec59bc18b8079d199706ff0a710609b9ff3d7d
Blake <blake.breon@cuw.edu>
2022-05-02 14:57:11 -0500

    Update

--- Assets/Scripts/PacManScript.cs
+++ Assets/Scripts/PacManScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PacMacScript : MonoBehaviour
+public class PacManScript : MonoBehaviour
 {
     Rigidbody rb;
     NavMeshAgent pinkGhostAgent;
